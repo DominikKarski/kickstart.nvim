f2cc5cd40258886a2159a8af305043a451dd36da
DominikKarski <dominik-karski@wp.pl>
2024-07-22 16:40:38 +0200

    Add nvim-treesitter-context and oil.nvim plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,4 +16,22 @@ return {
       cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      mode = 'cursor',
+      max_lines = 3,
+    },
+  },
+  {
+    'stevearc/oil.nvim',
+    config = function ()
+      require('oil').setup {
+        columns = {'size'},
+        view_options = {
+          show_hidden = true,
+        },
+      }
+    end
+  }
 }
